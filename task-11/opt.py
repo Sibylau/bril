f375bd79962d18901ded6808293285aa3b19f9db
Sibylau <jl3952@cornell.edu>
2020-12-08 00:09:22 -0500

    [JIT] finish opt and stitch

--- task-11/opt.py
+++ task-11/opt.py
@@ -3,21 +3,15 @@ import sys
 from modelcfg import *
 
 def simple_dse(lines):
-  print(lines)
   change = True
   while change:
     change = False
     used = []
     for instr in lines:
-      dict_instr = json.loads(instr)
-      if isinstance(dict_instr, dict) and 'args' in dict_instr:
-        # print(instr)
-        used.extend(dict_instr['args'])
-    print(used)
+      if 'args' in instr:
+        used.extend(instr['args'])
     for instr in lines:
-      dict_instr = json.loads(instr)
-      if isinstance(dict_instr, dict) and \
-        'dest' in dict_instr and dict_instr['dest'] not in used:
+      if 'dest' in instr and instr['dest'] not in used:
         lines.remove(instr)
         change = True
   return lines
@@ -25,7 +19,14 @@ def simple_dse(lines):
 if __name__ == "__main__":
   file_name = sys.argv[1]
   trace_file = open(file_name, 'r')
-  lines = trace_file.read().splitlines()
+  lines = []
+  while True:
+    line = trace_file.readline()
+    if len(line) == 0:
+      break
+    line = json.loads(line)
+    if isinstance(line, dict):
+      lines.append(line)
   instrs = simple_dse(lines)
   print(instrs)
   trace_file.close()
--- task-11/stitch.py
+++ task-11/stitch.py
@@ -3,17 +3,35 @@ import sys
 from modelcfg import *
 from opt import *
 
-def stitch(instrs, source_file):
-  
+def stitch(instrs, prog):
+  for func in prog['functions']:
+    if func['name'] == "main":
+      instr = {"op": "commit"}
+      instrs.append(instr)
+      instr = {"op": "ret"}
+      instrs.append(instr)
+      instr = {"label": "failed"}
+      instrs.append(instr)
+      func['instrs'] = instrs + func['instrs']
+      instr = {"op": "speculate"}
+      func['instrs'].insert(0, instr)
+        
+  return prog
 
 if __name__ == "__main__":
   trace_name = sys.argv[1]
   trace_file = open(trace_name, 'r')
-  source_name = sys.argv[2]
-  source_file = open(source_name, 'wr')
-  prog = json.load(source_file)
-  lines = trace_file.read().splitlines()
+  lines = []
+  while True:
+    line = trace_file.readline()
+    if len(line) == 0:
+      break
+    line = json.loads(line)
+    if isinstance(line, dict):
+      lines.append(line)
   instrs = simple_dse(lines)
-  stitch(instrs, source_file)
+
+  prog = json.load(sys.stdin)
+  stitch(instrs, prog)
   trace_file.close()
-  source_file.close()
+  print(json.dumps(prog))
